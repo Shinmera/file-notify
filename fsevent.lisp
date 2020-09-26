b7c50f72899ebb94d796a88ef4ad3ac381fe1436
Shinmera <shinmera@tymoon.eu>
2020-09-26 15:53:08 +0200

    Stub

---
+++ fsevent.lisp
@@ -0,0 +1,8 @@
+#|
+ This file is a part of file-notify
+ (c) 2020 Shirakumo http://tymoon.eu (shinmera@tymoon.eu)
+ Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(in-package #:org.shirakumo.file-notify)
+
