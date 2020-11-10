28af23c51db4e8a1d55a7c53cb077d44f57a7c68
Shinmera <shinmera@tymoon.eu>
2020-11-10 15:36:11 +0100

    Switch to using overlapped result stuff.

--- windows.lisp
+++ windows.lisp
@@ -21,6 +21,28 @@
   (:security         #x00000100)
   (:all              #x0000011F))
 
+(cffi:defbitfield (access-flag :uint32)
+  (:directory #x1)
+  (:read #x80000000))
+
+(cffi:defbitfield (share-flag :uint32)
+  (:none #x0)
+  (:read #x1)
+  (:write #x2)
+  (:delete #x4))
+
+(cffi:defcenum (disposition-flag :uint32)
+  (:new 1)
+  (:create 2)
+  (:existing 3)
+  (:always 4)
+  (:truncate 5))
+
+(cffi:defbitfield (create-flag :uint32)
+  (:normal #x80)
+  (:backup #x02000000)
+  (:overlapped #x40000000))
+
 (cffi:defcenum (action :uint32 :allow-undeclared-values T)
   (:create 1)
   :delete
@@ -45,6 +67,19 @@
   (length :uint32)
   (name :uint16 :count 259))
 
+(cffi:defcstruct (overlapped :conc-name overlapped-)
+  (internal :size)
+  (internal-high :size)
+  (offset :uint32)
+  (offset-high :uint32)
+  (handle :pointer))
+
+(cffi:defcfun (overlapped-result "GetOverlappedResult") :bool
+  (handle :pointer)
+  (overlapped :pointer)
+  (bytes :pointer)
+  (wait :bool))
+
 (cffi:defcfun (first-change "FindFirstChangeNotificationW") :pointer
   (path :pointer)
   (watch-subtree :bool)
@@ -54,6 +89,24 @@
   (handle :pointer))
 
 (cffi:defcfun (close-change "FindCloseChangeNotification") :bool
+  (handle :pointer))
+
+(cffi:defcfun (create-event "CreateEventA") :pointer
+  (security-attributes :pointer)
+  (reset :bool)
+  (state :bool)
+  (name :string))
+
+(cffi:defcfun (create-file "CreateFileW") :pointer
+  (path :pointer)
+  (access access-flag)
+  (share share-flag)
+  (security-attributes :pointer)
+  (creation-disposition disposition-flag)
+  (flags create-flag)
+  (template :pointer))
+
+(cffi:defcfun (close-handle "CloseHandle") :bool
   (handle :pointer))
 
 (cffi:defcfun (read-changes "ReadDirectoryChangesW") :bool
@@ -82,35 +135,52 @@
 
 (define-implementation init ())
 
-(defstruct (watch (:constructor make-watch (handle table)))
-  handle table)
+(defstruct (watch (:constructor make-watch (directory handle event buffer overlapped table)))
+  directory handle event buffer overlapped table)
 
 (define-implementation shutdown ()
   (loop for v being the hash-values of *watches*
-        do (close-change (watch-handle v)))
+        do (close-handle (watch-handle v))
+           (cffi:foreign-free (watch-event v))
+           (cffi:foreign-free (watch-overlapped v)))
   (clrhash *watches*))
 
 (define-implementation watch (file/s &key (events T))
-  (let ((filter (case events
-                  ((T) :all)
-                  ((NIL) ())
-                  (T events))))
-    (flet ((add (path)
-             (with-simple-restart (abort "Don't watch ~a" path)
-               (let* ((path (truename path))
-                      (dir (make-pathname :name NIL :type NIL :defaults path))
-                      (existing (gethash dir *watches*)))
-                 (unless existing
-                   (com:with-wstring (path (namestring dir))
-                     (let ((handle (first-change path NIL filter)))
-                       (check-last-error (/= #+64-bit #xFFFFFFFFFFFFFFFF #-64-bit #xFFFFFFFF
-                                             (cffi:pointer-address handle)))
-                       (setf existing (make-watch handle (make-hash-table :test 'equal)))
-                       (setf (gethash dir *watches*) existing))))
-                 (setf (gethash path (watch-table existing)) T)))))
-      (if (listp file/s)
-          (mapc #'add file/s)
-          (add file/s)))))
+  (declare (ignore events))
+  (flet ((add (path)
+           (with-simple-restart (abort "Don't watch ~a" path)
+             (let* ((path (truename path))
+                    (dir (make-pathname :name NIL :type NIL :defaults path))
+                    (existing (gethash dir *watches*)))
+               (unless existing
+                 (com:with-wstring (path (namestring dir))
+                   (let ((handle (create-file path
+                                              :directory
+                                              '(:read :write :delete)
+                                              (cffi:null-pointer)
+                                              :existing
+                                              '(:backup :overlapped)
+                                              (cffi:null-pointer)))
+                         (event (create-event (cffi:null-pointer)
+                                              NIL
+                                              NIL
+                                              (cffi:null-pointer))))
+                     (check-last-error (/= #+64-bit #xFFFFFFFFFFFFFFFF #-64-bit #xFFFFFFFF
+                                           (cffi:pointer-address handle)))
+                     (check-last-error (/= #+64-bit #xFFFFFFFFFFFFFFFF #-64-bit #xFFFFFFFF
+                                           (cffi:pointer-address event)))
+                     (setf existing (make-watch (namestring dir)
+                                                handle
+                                                event
+                                                (cffi:foreign-alloc '(:struct event))
+                                                (cffi:foreign-alloc '(:struct overlapped))
+                                                (make-hash-table :test 'equal)))
+                     (start-read existing)
+                     (setf (gethash dir *watches*) existing))))
+               (setf (gethash path (watch-table existing)) T)))))
+    (if (listp file/s)
+        (mapc #'add file/s)
+        (add file/s))))
 
 (define-implementation list-watched ()
   (let ((watched ()))
@@ -127,25 +197,34 @@
              (when existing
                (remhash path (watch-table existing))
                (when (= 0 (hash-table-count (watch-table existing)))
-                 (check-last-error (close-change (watch-handle existing)))
+                 (close-handle (watch-handle existing))
+                 (close-handle (watch-event existing))
+                 (cffi:foreign-free (watch-buffer existing))
+                 (cffi:foreign-free (watch-overlapped existing))
                  (remhash dir *watches*))))))
     (if (listp file/s)
         (mapc #'del file/s)
         (del file/s))))
 
-(defun process (handle directory function)
-  (cffi:with-foreign-objects ((event '(:struct event))
-                              (written :uint32))
-    (check-last-error (read-changes handle event (cffi:foreign-type-size '(:struct event)) NIL :all
-                                    written (cffi:null-pointer) (cffi:null-pointer)))
-    (let ((path (merge-pathnames (com:wstring->string (cffi:foreign-slot-pointer event '(:struct event) 'name)
-                                                      (event-length event))
-                                 directory))
-          (action (event-action event)))
-      (when (keywordp action)
-        (with-simple-restart (abort "Don't notify the change to ~a" path)
-          (funcall function path action))))
-    (check-last-error (next-change handle))))
+(defun start-read (watch)
+  (with-slots (handle event buffer overlapped) watch
+    (setf (overlapped-internal overlapped) 0)
+    (setf (overlapped-internal-high overlapped) 0)
+    (setf (overlapped-offset overlapped) 0)
+    (setf (overlapped-offset-high overlapped) 0)
+    (setf (overlapped-handle overlapped) event)
+    (check-last-error (read-changes handle buffer (cffi:foreign-type-size '(:struct event)) NIL :all
+                                    (cffi:null-pointer) overlapped (cffi:null-pointer)))))
+
+(defun process-change (watch function)
+  (let* ((buffer (watch-buffer watch))
+         (path (merge-pathnames (com:wstring->string (cffi:foreign-slot-pointer buffer '(:struct event) 'name)
+                                                     (event-length buffer))
+                                (watch-directory watch)))
+         (action (event-action buffer)))
+    (when (keywordp action)
+      (with-simple-restart (abort "Don't notify the change to ~a" path)
+        (funcall function path action)))))
 
 (define-implementation process-events (function &key timeout)
   (let* ((tsec (etypecase timeout
@@ -154,24 +233,27 @@
                  ((real 0) timeout)))
          (msec (floor (* 1000 tsec)))
          (count (hash-table-count *watches*))
-         (dirs (make-array count)))
+         (watches (make-array count)))
     (when (<= #x80 count)
       (error "FIXME: Too many watches. Don't know how to deal with this."))
     (when (< 0 count)
+      ;; First check whether we have any pending overlaps
       (cffi:with-foreign-objects ((handles :pointer count))
         (loop for i from 0
-              for directory being the hash-keys of *watches*
               for watch being the hash-values of *watches*
-              do (setf (cffi:mem-aref handles :pointer i) (watch-handle watch))
-                 (setf (aref dirs i) directory))
+              do (setf (cffi:mem-aref handles :pointer i) (watch-event watch))
+                 (setf (aref watches i) watch))
+        ;; Next watch for changes
         (loop for ev = (wait-for-multiple-objects count handles NIL msec)
               do (case ev
                    ((:abandoned :io-completion))
                    (:timeout
+                    (finish-output)
                     (unless (eql T timeout)
                       (return)))
                    (:failed
                     (check-last-error NIL))
                    (T
                     (when (< ev #x80)
-                      (process (cffi:mem-aref handles :pointer ev) (aref dirs ev) function)))))))))
+                      (process-change (aref watches ev) function)
+                      (start-read (aref watches ev))))))))))
